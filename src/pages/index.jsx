612e709b5a7089adb3b23da89f42afd0ecf5f360
pedroksty <pedrotrab2132@gmail.com>
2020-11-20 15:09:19 -0300

    Create Components in ApresentaionContainer

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,26 +1,36 @@
 import Image from 'next/image'
 import SEO from '@/components/SEO'
 import { useEffect } from 'react'
+import Typist from 'react-typist'
 
 import {
   Container,
   Header,
-  NavText
+  HeaderImage,
+  NavText,
+  NavWrapper,
+  ApresentationWrapper,
+  TextContainer,
+  SecondImage,
+  TextApresentation
 } from '../styles/pages/Home'
 
+import logoBranca from '../../public/logo-branca.png'
+import coffe from '../assets/coffe.svg'
+
 export default function Home() {
-  useEffect(() => {
-  (function(s, z, c, h, a, t) {
-    s.SZchat = s.SZchat || function() {
-      (s.SZchat.q = s.SZchat.q || []).push(arguments)
-    }
-    t = z.createElement(c),
-      a = z.getElementsByTagName(c)[0]; t.async = 1
-    t.src = h + '/webchat/v2/szchat.js'; a.parentNode.insertBefore(t, a)
-    s.SZchat('cid', '5f736baa8aff6b5f29188083')
-    s.SZchat('host', h)
-  })(window, document, 'script', 'https://interas.sz.chat')
-  })
+  // useEffect(() => {
+  // (function(s, z, c, h, a, t) {
+  //   s.SZchat = s.SZchat || function() {
+  //     (s.SZchat.q = s.SZchat.q || []).push(arguments)
+  //   }
+  //   t = z.createElement(c),
+  //     a = z.getElementsByTagName(c)[0]; t.async = 1
+  //   t.src = h + '/webchat/v2/szchat.js'; a.parentNode.insertBefore(t, a)
+  //   s.SZchat('cid', '5f736baa8aff6b5f29188083')
+  //   s.SZchat('host', h)
+  // })(window, document, 'script', 'https://interas.sz.chat')
+  // })
 
   return (
     <Container>
@@ -33,13 +43,55 @@ export default function Home() {
 
       <Header>
 
+          <img src={logoBranca} alt="Interas"/>
+
+        <NavWrapper>
+
         <NavText>Início</NavText>
         <NavText>Quem Somos?</NavText>
         <NavText>Soluções</NavText>
         <NavText>Produtos</NavText>
         <NavText>Contato</NavText>
 
+        </NavWrapper>
+
       </Header>
+      <ApresentationWrapper>
+        <TextContainer >
+          <TextApresentation> Procurando soluções em </TextApresentation>
+          <Typist stdTypingDelay={15} cursor={{ show: true, blink: true }} >
+          <TextApresentation > Automoção de chat ?</TextApresentation>
+          <Typist.Backspace count={20} delay={400} />
+          <TextApresentation> HotSpot ? </TextApresentation>
+          <Typist.Backspace count={9} delay={400} />
+          <TextApresentation>elp Desk ? </TextApresentation>
+
+          </Typist>
+
+          <p>
+            Somos uma empresa integradora de
+            soluções e consultoria em Tecnologia.
+            Unimos novas idéias à novas tecnologias e
+            oferecemos um olhar único para sua necessidade
+            tecnológica.
+          </p>
+
+          <button onClick={() => {}} >Vamos começar</button>
+
+        </TextContainer>
+
+        <SecondImage
+          src="/homepage-window.png" alt="coffe"
+        />
+
+        <aside>
+
+        <img
+          src="/apresentationImage.svg" alt="coffe"
+        />
+        </aside>
+
+      </ApresentationWrapper>
 
     </Container>
   )
--- src/styles/pages/Home.ts
+++ src/styles/pages/Home.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import Image from 'next/image'
 
 export const Container = styled.div`
 
@@ -14,9 +15,27 @@ export const Header = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
+  justify-content: space-evenly;
+
+  > img {
+    cursor: pointer;
+    transition:  0.2s;
+
+    width: 212px;
+    height: 55px;
+
+
+    &:active {
+      width: 175px;
+      height: 45px;
+    }
+  }
+`
+
+export const NavWrapper = styled.nav`
+  display: flex;
+  align-items: center;
   justify-content: center;
-
-
 `
 
 export const NavText = styled.p`
@@ -30,4 +49,86 @@ export const NavText = styled.p`
     color: #fff;
   }
 
+`
+
+export const ApresentationWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  height: 55vh;
+  padding: 20px 200px 0px;
+  background: #143954;
+  background-image: url('/homepage-intro-pattern.png');
+
+  background-size: auto 70%;
+  background-repeat: repeat-x;
+
+  > img {
+
+    width: 378px;
+    height: 390px;
+    position: absolute;
+    top: 64px;
+    right: 720px;
+  }
+
+  > aside {
+      img {
+        width: 600px;
+        height: 214px;
+        position: absolute;
+        bottom: 420px;
+        right: 450px;
+      }
+
+  }
+
+
+
+`
+
+export const SecondImage = styled.img`
+    width: 600px;
+    height: 260px;
+    position: absolute;
+    bottom: 240px;
+    right: 150px;
+`
+
+export const TextContainer = styled.div`
+  width: 600px;
+  z-index: 92828;
+
+  > p {
+    font: 400 22px Raleway, Sans-serif;
+    margin-top: 24px;
+    max-width: 370px;
+    z-index: 200;
+    color: ${({ theme }) => theme.colors.textSecondWhite}
+
+  }
+
+  > button {
+      margin-top: 12px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding: 10px 16px;
+      background: #40a9bd;
+      border-radius: 6px;
+      width: 143px;
+      height: 40px;
+      font: 500 14px Raleway, Sans-serif;
+      cursor: pointer;
+      color: #fff;
+      transition: background 0.3s;
+
+      &:hover {
+        background: #4abdd2;
+      }
+  }
+
+`
+export const TextApresentation = styled.strong`
+    font: 500 48px Raleway, Sans-serif;
+    color: ${({ theme }) => theme.colors.textWhite};
 `
